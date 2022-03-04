9e2cec2fb8f334ea104c0fa4320309456df8c4ed
Adam Toennis <adam@atomicrobot.com>
2022-03-04 10:51:01 -0500

    Initial Commit

---
+++ Shared/ble_demo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ble_demo_iosApp.swift
+//  Shared
+//
+//  Created by Adam Toennis on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ble_demo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
